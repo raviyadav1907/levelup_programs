0da44967bdbd953ccd39a7bd949e74bea95349f4
Ravi Yadav <raviy.ic.17@nsit.net.in>
2020-05-09 10:02:46 +0530

    updated

---
+++ recursion/lec_06/bits.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+    //on -> on;
+    //off -> on;
+    int bit_off_to_on(int num, int k){
+        int mask = (1<<k);
+        return (num | mask);
+    }
+
+    //off -> off;
+    //on -> off;
+    int bit_on_to_off(int num, int k){
+        int mask = (~(1<<k)); // 1's compliment of mask;
+        return (num & mask);
+    }
+
+    int onToOff_02(int num, int k)
+{
+    int mask = (1 << k); //1's compliment of mask.
+    if ((num & mask) == 0)
+        return num;
+    else
+        return (num ^ mask);
+}
+
+int countAllSetBits_02(unsigned_int num)
+{
+    int count = 0;
+    while (num != 0)
+    {
+        if ((num & 1) != 0)
+            count++;
+        num >>= 1;
+    }
+}
+
+//leetcode: 338===========================================
+
+    vector<int> countBits(int num)
+    {
+        vector<int> ans(num+1,0);
+        for(int i=1;i<=num;i++) ans[i]=ans[(i & (i-1))] + 1;
+        return ans;
+    }
+    
+    int main(){
+        int num = 123;
+        int k = 3;
+        cout << (bit_on_to_off(num, k) == onToOff_02(num, k)) << endl;
+        return 0;
+    }
--- recursion/lec_06/lec06.java
+++ recursion/lec_06/lec06.java
@@ -5,11 +5,92 @@ import java.util.*;
 class lec06{
 
     public static void main(String[] args){
-
+        solution();
     }
 
     public static void solution(){
-        
+        //Nqueen_optimitation();
+        Nqueen_optimitation_bits();
+    }
+
+    public static void Nqueen_optimitation(){
+        int tnq = 4 ;
+        int n = 4 ;
+        int m = 4 ;
+        row = new boolean[n];
+        col = new boolean[m];
+        diag = new boolean[n+m-1];
+        antidiag = new boolean[m+n-1];
+        System.out.println(Nqueen_Optimized(n, m, 0, tnq, ""));
+    }
+
+    public static void Nqueen_optimitation_bits(){
+        int tnq = 12 ;
+        int n = 12 ;
+        int m = 12 ;
+        System.out.println(Nqueen_Optimized_bits(n, m, 0, tnq, ""));
+    }
+
+// NQueen Optimized=====================================================================================================================================================
+
+    public static boolean[] row;
+    public static boolean[] col;
+    public static boolean[] diag;
+    public static boolean[] antidiag;
+
+    public static int Nqueen_Optimized(int n, int m, int idx, int tnq, String ans){
+        if(tnq==0){
+            System.out.println(ans);
+            return 1;
+        }
+        int count = 0;
+        for (int i = idx; i < n*m ; i++){
+            int x = i / m;
+            int y = i % m;
+            if(!row[x] && !col[y] && !diag[x+y] && !antidiag[x-y+(m-1)]){
+                row[x]=true;
+                col[y]=true;
+                diag[x+y]=true;
+                antidiag[x-y+m-1]=true;
+                count += Nqueen_Optimized(n,m, i+1,tnq-1, ans + "(" + x + ", " + y + ") ");
+                row[x]=false;
+                col[y]=false;
+                diag[x+y]=false;
+                antidiag[x-y+m-1]=false;
+            }
+        }
+        return count;
+    }
+
+// Nqueen using Bitmasking==============================================================================================================================================
+
+    public static int row_bit = 0;
+    public static int col_bit = 0;
+    public static int diag_bit = 0;
+    public static int antidiag_bit = 0;
+
+    public static int Nqueen_Optimized_bits(int n, int m, int idx, int tnq, String ans){
+        if(tnq==0){
+            System.out.println(ans);
+            return 1;
+        }
+        int count = 0;
+        for (int i = idx; i < n*m ; i++){
+            int x = i / m;
+            int y = i % m;
+            if((row_bit &(1<<x))==0 && (col_bit &(1<<y))==0 && (diag_bit &(1<<(x+y)))==0 && (antidiag_bit&(1<<(x-y+m-1)))==0){
+                row_bit^=(1<<x);
+                col_bit^=(1<<y);
+                diag_bit^=(1<<(x+y));
+                antidiag_bit^=(1<<(x-y+m-1));
+                count += Nqueen_Optimized_bits(n,m, i+1,tnq-1, ans + "(" + x + ", " + y + ") ");
+                row_bit^=(1<<x);
+                col_bit^=(1<<y);
+                diag_bit^=(1<<(x+y));
+                antidiag_bit^=(1<<(x-y+m-1));
+            }
+        }
+        return count;
     }
 
 }
