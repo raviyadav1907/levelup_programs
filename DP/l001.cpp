a11f7bdcb77eeb6e8e3cca4fc9895e7f1b91bd46
Ravi Yadav <raviy.ic.17@nsit.net.in>
2020-06-25 23:55:19 +0530

    upadted

--- DP/l001.cpp
+++ DP/l001.cpp
@@ -425,6 +425,43 @@ int minPathSum(vector<vector<int>> &grid)
     // return minpathsum_tabu(row, col, grid);
 }
 
+// gfg goldmine_problem ==========================================================
+
+int getMaxGold(vector<vector<int>> gold, int m, int n)
+{
+    int goldTable[m][n];
+
+    for (int col = n - 1; col >= 0; col--)
+    {
+        for (int row = 0; row < m; row++)
+        {
+            int right = (col == n - 1) ? 0 : goldTable[row][col + 1];
+
+            int right_up = (row == 0 || col == n - 1) ? 0 : goldTable[row - 1][col + 1];
+
+            int right_down = (row == m - 1 || col == n - 1) ? 0 : goldTable[row + 1][col + 1];
+
+            goldTable[row][col] = gold[row][col] +
+                                  max(right, max(right_up, right_down));
+        }
+    }
+
+    int res = goldTable[0][0];
+    for (int i = 1; i < m; i++)
+        res = max(res, goldTable[i][0]);
+    return res;
+}
+
+void gfg_gold()
+{
+    vector<vector<int>> gold = {{1, 3, 1, 5},
+                    {2, 2, 4, 1},
+                    {5, 0, 2, 3},
+                    {0, 6, 1, 2}};
+    int m = 4, n = 4;
+    cout << getMaxGold(gold, m, n);
+}
+
 void set1()
 {
     int n, m;
